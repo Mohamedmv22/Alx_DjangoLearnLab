eb509ca03f446286cba93e71db915fbc548b8439
Mohamedmv22 <vevomohamed0@gmail.com>
2026-02-04 16:04:41 +0200

    Add permission protected views in bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/books.html', {'books': books})
+
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    return render(request, 'bookshelf/create.html')
+
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request):
+    return render(request, 'bookshelf/edit.html')
+
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request):
+    return render(request, 'bookshelf/delete.html')
